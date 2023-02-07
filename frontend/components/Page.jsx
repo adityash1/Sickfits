28ac810d272444942d91e2cf1e7b7fab19c3deb9
Aditya Sharma <aditya167411@gmail.com>
2023-02-07 23:28:43 +0530

    feat: global styles

--- frontend/components/Page.jsx
+++ frontend/components/Page.jsx
@@ -1,15 +1,23 @@
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import GlobalStyles from "./globalStyles";
 import Header from "./Header";
 
 const Page = ({ children }) => {
   return (
     <div>
+      <GlobalStyles />
       <Header />
-      <h2>Its a page component</h2>
-      {children}
+      <InnerStyles>{children}</InnerStyles>
     </div>
   );
 };
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 Page.propTypes = {
   children: PropTypes.node.isRequired,
