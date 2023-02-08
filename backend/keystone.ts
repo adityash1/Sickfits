cf7ab1055c764197650e132d3d2e14020edb0c12
Aditya Sharma <aditya167411@gmail.com>
2023-02-08 19:09:08 +0530

    setting up keystone

---
+++ backend/keystone.ts
@@ -0,0 +1,32 @@
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import "dotenv/config";
+
+const databaseURL = process.env.DATABASE_URL;
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  // @ts-ignore
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL as string],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: "mongoose",
+    url: databaseURL as string,
+    // TODO: Add data seeding here
+  },
+  lists: createSchema({
+    // Schema items go in here
+  }),
+  ui: {
+    // TODO: change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO: Add session values here
+});
