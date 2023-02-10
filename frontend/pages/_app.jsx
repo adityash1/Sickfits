f0d0d0babd08de179c632832520035c1869e54b9
Aditya Sharma <aditya167411@gmail.com>
2023-02-10 13:38:50 +0530

    fix

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -10,7 +10,6 @@ Router.events.on("routeChangeComplete", () => nProgress.done());
 Router.events.on("routeChangeError", () => nProgress.done());
 
 const MyApp = ({ Component, pageProps, apollo }) => {
-  console.log(apollo);
   return (
     <ApolloProvider client={apollo}>
       <Page>
@@ -18,6 +17,15 @@ const MyApp = ({ Component, pageProps, apollo }) => {
       </Page>
     </ApolloProvider>
   );
+};
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
 };
 
 export default withData(MyApp);
