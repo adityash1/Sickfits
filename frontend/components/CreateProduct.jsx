9d50260fbce4244dbb07946c0e6d37a1b542d9ba
Aditya Sharma <aditya167411@gmail.com>
2023-02-11 02:26:28 +0530

    fix: tweaks to form validation

--- frontend/components/CreateProduct.jsx
+++ frontend/components/CreateProduct.jsx
@@ -13,21 +13,44 @@ export default function CreateProduct() {
     <Form onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="Image">
         Image
-        <input type="file" {...register("Image", { required: true })} />
-        {errors.Image && <div>This field is required</div>}
+        <input
+          type="file"
+          {...register("Image", { required: true })}
+          aria-invalid={errors.firstName ? "true" : "false"}
+        />
+        {errors.firstName?.type === "required" && (
+          <p role="alert">Image is required</p>
+        )}
+      </label>
+      <label htmlFor="Name">
         Name
-        <input {...register("Name", { required: true })} />
-        {errors.Name && <span>This field is required</span>}
+        <input
+          {...register("Name", { required: true })}
+          aria-invalid={errors.Name ? "true" : "false"}
+        />
+        {errors.Name?.type === "required" && (
+          <p role="alert">Name is required</p>
+        )}
       </label>
       <label htmlFor="Price">
         Price
-        <input {...register("Price", { required: true })} />
-        {errors.Price && <span>This field is required</span>}
+        <input
+          {...register("Price", { required: true })}
+          aria-invalid={errors.Price ? "true" : "false"}
+        />
+        {errors.Price?.type === "required" && (
+          <p role="alert">Price is required</p>
+        )}
       </label>
       <label htmlFor="Description">
         Description
-        <textarea {...register("Description", { required: true })} />
-        {errors.Description && <span>This field is required</span>}
+        <textarea
+          {...register("Description", { required: true })}
+          aria-invalid={errors.Description ? "true" : "false"}
+        />
+        {errors.Description?.type === "required" && (
+          <p role="alert">Description is required</p>
+        )}
       </label>
       <input type="submit" />
     </Form>
