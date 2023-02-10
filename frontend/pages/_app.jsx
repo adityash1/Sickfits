6a452d3706adce1247dd6d6a4035ac231f79d0e6
Aditya Sharma <aditya167411@gmail.com>
2023-02-10 13:20:34 +0530

    chore: setup apollo client

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -2,17 +2,22 @@ import Page from "@/components/Page";
 import { Router } from "next/router";
 import nProgress from "nprogress";
 import "../components/styles/nprogress.css";
+import { ApolloProvider } from "@apollo/client";
+import withData from "@/lib/withData";
 
 Router.events.on("routeChangeStart", () => nProgress.start());
 Router.events.on("routeChangeComplete", () => nProgress.done());
 Router.events.on("routeChangeError", () => nProgress.done());
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps, apollo }) => {
+  console.log(apollo);
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 };
 
-export default MyApp;
+export default withData(MyApp);
