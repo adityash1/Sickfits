deeb0cc871fd12350e2e4452d924f1b43ed8b9ac
Aditya Sharma <aditya167411@gmail.com>
2023-02-11 02:18:40 +0530

    chore: removed unused css

--- frontend/components/styles/Form.js
+++ frontend/components/styles/Form.js
@@ -46,29 +46,6 @@ const Form = styled.form`
     font-weight: 600;
     padding: 0.5rem 1.2rem;
   }
-  fieldset {
-    border: 0;
-    padding: 0;
-
-    &[disabled] {
-      opacity: 0.5;
-    }
-    &::before {
-      height: 10px;
-      content: "";
-      display: block;
-      background-image: linear-gradient(
-        to right,
-        #ff3019 0%,
-        #e2b04a 50%,
-        #ff3019 100%
-      );
-    }
-    &[aria-busy="true"]::before {
-      background-size: 50% auto;
-      animation: ${loading} 0.5s linear infinite;
-    }
-  }
 `;
 
 export default Form;
